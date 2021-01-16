88ef46079400732330576197c9e363abb8f026d4
Ariel Young <arielyoung103@gmail.com>
2021-01-16 04:02:43 -0800

    Made the driver itself work (in lidar_scanner.rs), added example visualizer using sdl2.

--- src/lidar_scanner.rs
+++ src/lidar_scanner.rs
@@ -20,7 +20,7 @@ pub struct LidarScanner {
 
 impl LidarScanner {
     pub fn new(port: &str, baud_rate: u32) -> Self {
-        let mut port = serialport::new(port, baud_rate)
+        let mut port = serialport::new(port, baud_rate.clone())
             .timeout(Duration::from_millis(500))
             .open()
             .expect(&*format!("Unable to open serial port {}", port));
@@ -47,21 +47,22 @@ impl LidarScanner {
 
     pub fn poll(&mut self) -> [u16; 360] {
         let mut got_scan = false;
-        let mut index = 0_i32;
-        let mut start_count = 0_usize;
+        let mut index = 0_usize;
         let mut motor_speed = 0_u32;
         let mut rpms = 0_u16;
 
-        while !self.shutting_down && !got_scan {
-            self.serial_port.read_exact(&mut self.raw_bytes[..1]);
+        // self.range_data = [0; 360];
+        // self.intensity_data = [0; 360];
 
-            if start_count == 0 && self.raw_bytes[start_count] == 0xFA {
-                start_count = 1;
-            } else if start_count == 1 {
-                if self.raw_bytes[start_count] == 0xA0 {
-                    start_count = 0;
+        while !self.shutting_down && !got_scan {
+            // self.serial_port.read_exact(&mut self.raw_bytes[..2]);
+            // println!("{:x?}, {:x?}", self.raw_bytes[0], self.raw_bytes[1]);
+            self.serial_port.read_exact(&mut self.raw_bytes[0..1]);
+            if self.raw_bytes[0] == 0xFA {
+                self.serial_port.read_exact(&mut self.raw_bytes[1..2]);
+                if self.raw_bytes[1] == 0xA0 {
                     got_scan = true;
-                    let res = self.serial_port.read_exact(&mut self.raw_bytes[1..]);
+                    let res = self.serial_port.read_exact(&mut self.raw_bytes[2..]);
                     match res {
                         Ok(_) => {}
                         Err(e) => {
@@ -70,23 +71,25 @@ impl LidarScanner {
                     }
 
                     for i in (0..self.raw_bytes.len()).step_by(42) {
-                        motor_speed += ((self.raw_bytes[i+3] as u32) << 8) + self.raw_bytes[i+2] as u32;
-                        rpms = ((self.raw_bytes[i + 3] as u16) << 8_u8 | self.raw_bytes[i + 2] as u16) / 10;
+                        // if self.raw_bytes[i] == 0xFA && self.raw_bytes[i+1] == (0xA0 + (i as u8) / 42) {
+                            // motor_speed += ((self.raw_bytes[i + 3] as u32) << 8) + self.raw_bytes[i + 2] as u32;
+                            // rpms = ((self.raw_bytes[i + 3] as u16) << 8_u8 | self.raw_bytes[i + 2] as u16) / 10;
 
-                        for j in (i+4..i+40).step_by(6) {
-                            index = (6 * (i / 42) + (j - 4 - i) / 6) as i32;
+                            for j in ((i + 4)..(i + 40)).step_by(6) {
+                                index = (6 * (i / 42) + (j - 4 - i) / 6) as usize;
+                                let byte0 = self.raw_bytes[j] as u16;
+                                let byte1 = self.raw_bytes[j + 1] as u16;
+                                let byte2 = self.raw_bytes[j + 2] as u16;
+                                let byte3 = self.raw_bytes[j + 3] as u16;
 
-                            let byte0 = self.raw_bytes[j] as u16;
-                            let byte1 = self.raw_bytes[j + 1] as u16;
-                            let byte2 = self.raw_bytes[j + 2] as u16;
-                            let byte3 = self.raw_bytes[j + 3] as u16;
+                                println!("i[{}]: {}", index, self.intensity_data[index]);
+                                println!("r[{}]: {}", index, self.range_data[index]);
 
-                            self.intensity_data[359 - index as usize] = (byte1 << 8) + byte0;
-                            self.range_data[359 - index as usize] = (byte3 << 8) + byte2;
-                        }
+                                self.intensity_data[index as usize] = (byte1 << 8) + byte0;
+                                self.range_data[index as usize] = (byte3 << 8) + byte2;
+                            }
+                        // }
                     }
-                } else {
-                    start_count = 0
                 }
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,80 @@
 mod lidar_scanner;
-extern crate ctrlc;
 
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
+extern crate sdl2;
 
-fn main() {
-    let mut scanner = lidar_scanner::LidarScanner::new("/dev/ttyUSB0", 230400);
+use core::ops::Mul;
 
-    let should_cont = Arc::new(AtomicBool::new(true));
-    let s = should_cont.clone();
-    ctrlc::set_handler( move || s.store(false, Ordering::SeqCst) );
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
 
-    while should_cont.load(Ordering::SeqCst) {
-        let data = scanner.poll();
+pub fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
 
-        for i in 0..20 {
-            for j in 0..18 {
-                print!("{}: {}, ", i*18 + j, data[i*18 + j])
+    let window = video_subsystem.window("rust-sdl2 demo", 831, 831)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut scanner = lidar_scanner::LidarScanner::new("/dev/ttyUSB0", 230_400);
+    let mut pts = Vec::new();
+
+    'running: loop {
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+
+        let r_max = 400_i32;
+        let mid = 416;
+        let i_data = scanner.poll();
+
+        for (i, pt) in i_data.iter().enumerate() {
+            let pt = *pt;
+
+            if pt > 0 {
+                let cos_i = (i as f32).to_radians().cos();
+                let sin_i = (i as f32).to_radians().sin();
+                let rad = (pt as f32) / 4000.0 * (r_max as f32);
+
+                let x = (cos_i * rad) as i32 + mid;
+                let y = (sin_i * rad) as i32 + mid;
+
+                pts.push(sdl2::rect::Rect::from_center(
+                    sdl2::rect::Point::new(x, y),
+                    2, 2
+                ));
             }
-            println!()
         }
+        if pts.len() > 361 {
+            pts.drain(0..(pts.len() - 361));
+        }
+
+        canvas.set_draw_color(Color::RGB(255, 255, 255));
+        canvas.draw_rects(&pts[..]);
+        canvas.set_draw_color(Color::RGB(0, 255, 0));
+        canvas.draw_rect(sdl2::rect::Rect::new(mid, mid, 10, 10));
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                Event::KeyDown {keycode: Some(Keycode::A), ..} => {
+                    println!("asdf");
+                    pts.clear();
+                }
+                _ => {}
+            }
+        }
+
+        canvas.present();
     }
+    scanner.drop()
 }
